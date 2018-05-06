b899967de4c3853f79d499666cb6151a577e8dcc
xyyx <xyyx@mail.ru>
2018-05-06 10:28:28 +0800

    [DNM]msm8956-common: re-enable sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,7 +206,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wifi
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
