d5433e7171fca1c91379e340bd20dac4cb338693
Carlos Arriaga <37526095+TheStrechh@users.noreply.github.com>
2018-05-31 07:24:08 -0500

    msm8956-common: disable sepolicy for treble
    * building in permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -209,8 +209,8 @@ PROTOBUF_SUPPORTED := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wifi
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
